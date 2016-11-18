149f2359afc97e0a09c0e55e269058d2933106c2
jonny22094 <qwsazxxzaswq@wp.pl>
2016-11-19 00:09:30 +0100

    DO-IT
    not complete animation

--- scripts/mechanic.js
+++ scripts/mechanic.js
@@ -1,31 +1,59 @@
+var element = new Array(100);
 var quantity=0;
 var name;
 
+// Add new works
 
 document.getElementById('button').addEventListener('click', function(){
 
   name = document.getElementById('text').value;
   document.getElementById('text').value = "";
 
-  var div = document.createElement('div');
-  div.className += 'rectangle animated1 fadeIn';
+  if(name.length!=0){
 
-  document.getElementsByClassName('works_container')[0].appendChild(div);
+    ////////////           box          ////////////
+    var div = document.createElement('div');
+    div.className = 'rectangle animated1 fadeIn';
+    div.id += quantity;
+    document.getElementsByClassName('works_container')[0].appendChild(div);
 
-////////////////////////////////////////////////////////////////////
+    ////////////           text           ////////////
+    var inner = document.createElement('div');
+    inner.className += 'inner';
+    inner.innerHTML = name;
+    inner.id += quantity;
+    document.getElementsByClassName('rectangle')[quantity].appendChild(inner);
 
-  var inner = document.createElement('div');
-  inner.className += 'inner';
-  inner.innerHTML = name;
+    ////////////           remove           ////////////
+    var remove = document.createElement('div');
+    remove.className += 'remove';
+    remove.id += quantity+''+quantity;
+    div.setAttribute('onclick', 'remove_div('+quantity+')');
+    document.getElementsByClassName('rectangle')[quantity].appendChild(remove);
 
-  document.getElementsByClassName('rectangle')[quantity].appendChild(inner);
-
-////////////////////////////////////////////////////////////////////
-
-  var remove = document.createElement('div');
-  remove.className += 'remove';
-
-  document.getElementsByClassName('rectangle')[quantity].appendChild(remove);
-
-  quantity+=1;
+    quantity++;
+    op();
+  }
 });
+
+// Remove works
+function remove_div(g){
+  var element = document.getElementById(g);
+  element.className = 'rectangle animated1 fadeOut';
+
+  setTimeout(function(){
+    element.parentNode.removeChild(element);
+    quantity--;
+    op();
+  }, 250);
+}
+
+function op(){
+  var open = document.getElementById('open');
+
+  open.innerHTML = 'Open: '+quantity;
+  if(quantity<10) open.style.marginLeft = '220px';
+  else if(quantity<100) open.style.marginLeft = '210px';
+  else if(quantity<1000) open.style.marginLeft = '200px';
+
+}
